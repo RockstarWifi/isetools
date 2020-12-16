d0afd17b79ff2f18ebcb8db38e0574600fb9fd8f
Chris A <67589826+RockstarWifi@users.noreply.github.com>
2020-12-15 18:30:53 -0600

    ISETools-push.py
    A simple script to push Cisco IOS / IOS XE commands to devices using Python + Netmiko

---
+++ ISETools-push.py
@@ -0,0 +1,26 @@
+#Simple config push script for writing configs to Cisco IOS Devices
+#Part of ISETools by @RockstarWiFi - WiFiTraining.com
+#Requires text file 'devices' with list of IPv4 addresses you want to push code to, 1 per line, no spaces. 
+#Requires 'commands' text file with list of commands, no confg t needed, 1 command per line, no spaces.
+
+from netmiko import ConnectHandler
+
+with open('commands') as f:
+    commands_list = f.read().splitlines()
+
+with open('devices') as f:
+    devices_list = f.read().splitlines()
+
+for devices in devices_list:
+    print ('Connecting to device" ' + devices)
+    ip_address_of_device = devices
+    ios_device = {
+        'device_type': 'cisco_ios',
+        'ip': ip_address_of_device,
+        'username': 'chris',
+        'password': 'Cisco123'
+    }
+
+    net_connect = ConnectHandler(**ios_device)
+    output = net_connect.send_config_set(commands_list)
+    print (output)
